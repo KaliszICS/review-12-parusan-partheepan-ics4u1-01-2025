4fe44393e751ab4103825c758bf2eb6a05a9c816
parusan-partheepan-ics4u1-01-2025 <parusan.partheepan@student.tdsb.on.ca>
2025-09-23 17:04:19 +0000

    commit changes

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,6 +1,37 @@
 public class PracticeProblem {
 
-	public static void main(String args[]) {
+    public static void main(String args[]) {
 
-	}
+    }
+
+//Question 1
+public static boolean isPalindrome(String word) {
+    word = word.toLowerCase().replace(" ", "");
+    String rev = "";
+    for (int i = word.length() - 1; i >= 0; i--) {
+        rev = rev + word.charAt(i);
+    }
+            return word.equals(rev);
+
+}
+//Question 2
+public static int getAge(String[] names, int[] ages, String name) {
+    for (int i = 0; i < names.length; i++) {
+        if (names[i].equalsIgnoreCase(name)) {
+            return ages[i];
+        }
+    }
+    return -1;
+}
+//Question 3
+public static int countWords(String s1, char c1) {
+    int count = 0;
+    String[] words = s1.split(" ");
+    for (String word : words) {
+        if(word.toLowerCase().indexOf(Character.toLowerCase(c1)) != -1) { 
+            count++;
+    }
+}
+return count;
+}
 }
